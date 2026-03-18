324cb926c2c56e0b0a38fac295ce703a376f7736
Eyerusalem-Gebrat <eyerusalem.gebrat@gmail.com>
2026-03-18 23:39:49 +0300

    Time: 98 ms (25.75%), Space: 19.5 MB (54.3%) - LeetHub

---
+++ 0633-sum-of-square-numbers/0633-sum-of-square-numbers.py
@@ -0,0 +1,12 @@
+class Solution:
+    def judgeSquareSum(self, c: int) -> bool:
+        l, r = 0, int(c ** (1/2))
+        while l <= r:
+            if (l**2) + (r**2) == c:
+                return True
+            elif (l**2) + (r**2) < c:
+                l += 1
+            else:
+                r -= 1
+
+        return False
