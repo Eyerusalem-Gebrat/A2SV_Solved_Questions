3bce0c344e1cf808744fef6937d516a5393b17c7
Eyerusalem-Gebrat <eyerusalem.gebrat@gmail.com>
2026-03-24 13:38:43 +0300

    Add solution for 2121D-1709

--- codeforces/2121D-1709/2121D-1709.py
+++ codeforces/2121D-1709/2121D-1709.py
@@ -3,36 +3,36 @@ for _ in range(t):
   n = int(input())
   a = list(map(int, input().split()))
   b = list(map(int, input().split()))
-
+ 
   oprs = 0
   swaps = []
-
+ 
   for i in range(n):
     if a[i] > b[i]:
       a[i], b[i] = b[i], a[i]
       oprs += 1
       swaps.append((3, i+1))
-
+ 
   for j in range(n):
     for i in range(n-1):
       if a[i] > a[i+1]:
         a[i], a[i+1] = a[i+1], a[i]
         oprs += 1
         swaps.append((1, i+1))
-
+ 
   for j in range(n):
     for i in range(n-1):
       if b[i] > b[i+1]:
         b[i], b[i+1] = b[i+1], b[i]
         oprs += 1
         swaps.append((2, i+1))
-
+ 
   for i in range(n):
     if a[i] > b[i]:
       a[i], b[i] = b[i], a[i]
       oprs += 1
       swaps.append((3, i+1))
-
+ 
   print(oprs)
   for swap in swaps:
     print(*swap)
