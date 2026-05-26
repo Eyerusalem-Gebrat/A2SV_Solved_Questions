06392e0e2ee077a1adc2666b73ac3b1e74788b86
Eyerusalem-Gebrat <eyerusalem.gebrat@gmail.com>
2026-05-26 19:18:46 +0000

    Add solution for Count Integers With Even Digit Sum

---
+++ leetcode/2180-count-integers-with-even-digit-sum/2180-count-integers-with-even-digit-sum.py
@@ -0,0 +1,14 @@
+class Solution:
+    def countEven(self, num: int) -> int:
+        count = 0
+
+        for i in range(1, num + 1):
+            digit_sum = 0
+            x = i
+            while x > 0:
+                digit_sum += x % 10
+                x //= 10
+            if digit_sum % 2 == 0:
+                count += 1
+
+        return count
